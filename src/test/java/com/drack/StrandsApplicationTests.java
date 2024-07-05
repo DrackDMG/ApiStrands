d942a22cf8b6e5b5da1e6302f168e41e3dd5f575
Daniel Morales <danielmoralesgonzalez5@gmail.com>
2024-07-05 09:58:49 -0600

    Init commmit

---
+++ src/test/java/com/drack/StrandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
